acecc578c94a9955584163b23abada81b2e6214b
夕水 <854806732@qq.com>
2020-08-23 19:23:53 +0800

    完善了使用示例

---
+++ example/color-demo/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div id="color-picker"></div>
+  </div>
+</template>
+
+<script>
+import ewColorPicker from 'ew-color-picker';
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted(){
+    new ewColorPicker({
+      el:'#color-picker',
+      sure:(color) => {
+        this.$emit('on-sure-color',color);
+      },
+      clear:(color) => {
+        this.$emit('on-clear-color',color);
+      }
+    });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-bottom: 2em;
+  }
+  .hello > h1 {
+    margin-bottom: 15px;
+    color:#535455;
+  }
+</style>
---
+++ example/react-color-demo/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ewColorPicker from 'ew-color-picker';
+
+class ColorPicker extends React.Component {
+    constructor(props){
+      super(props);
+    }
+    componentDidMount(){
+      new ewColorPicker({
+        el:"#color-picker",
+        sure:(color) => {
+            this.props.changeBg(color);
+        },
+        clear:(color) => {
+          this.props.clearBg(color);
+        }
+      })
+    }
+    render(){
+      return (
+        <div id="color-picker"></div>
+      )
+    }
+}
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      color:"#fff"
+    }
+  }
+  changeBg(color){
+    this.setState({ color:color });
+  }
+  clearBg(color){
+    this.setState({ color:color });
+  }
+  render(){
+    return (
+      <div className="App" style={{ 'background':this.state.color }}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>欢迎使用ew-color-picker!</h1>
+        </header>
+        <main className="main">
+          <ColorPicker changeBg={this.changeBg.bind(this)} clearBg={this.clearBg.bind(this)}></ColorPicker>
+        </main>
+      </div>
+    )
+  }
+}
+
+
+export default App;
