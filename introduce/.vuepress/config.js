0d7c2a7a4a8dcdb76b867a50da84e2af7d31aa56
eveningwater <854806732@qq.com>
2020-11-21 18:23:03 +0800

    重新打包

--- introduce/.vuepress/config.js
+++ introduce/.vuepress/config.js
@@ -3,7 +3,7 @@ const sidebarConfig = require('./config/sidebarConfig.js')
 
 module.exports = {
     dest: "./docs",
-    base: "/",
+    base: "/ew-color-picker/",
     head: [
         ['link', {
             rel: 'icon',
