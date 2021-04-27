1e7db0cac470d4114a34fc1afbca00826585c76d
eveningwater <854806732@qq.com>
2021-04-27 23:04:06 +0800

    修改了依赖包的配置

--- dist/ew-color-picker.esm.js
+++ dist/ew-color-picker.esm.js
@@ -726,7 +726,7 @@ const baseDefaultConfig = {
   userDefineText: false
 };
 
-const consoleInfo = () => console.log(`%c ew-color-picker@1.9.1%c 联系QQ：854806732 %c 联系微信：eveningwater %c github:https://github.com/eveningwater/ew-color-picker %c `, 'background:#0ca6dc ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff', 'background:#ff7878 ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff', 'background:#ff7878 ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff', 'background:#ff7878 ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff', 'background:transparent');
+const consoleInfo = () => console.log(`%c ew-color-picker@1.9.3%c 联系QQ：854806732 %c 联系微信：eveningwater %c github:https://github.com/eveningwater/ew-color-picker %c `, 'background:#0ca6dc ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff', 'background:#ff7878 ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff', 'background:#ff7878 ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff', 'background:#ff7878 ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff', 'background:transparent');
 
 var zh = {
   clearText: "清空",
