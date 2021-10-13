9652a541356c1ff1a426cceb81f7639bf8734c06
eveningwater <854806732@qq.com>
2021-10-13 17:44:51 +0800

    添加了新的示例

---
+++ example/ew-color-picker-demo/main.ts
@@ -0,0 +1,6 @@
+import ewColorPicker from 'ew-color-picker';
+import "ew-color-picker/dist/ew-color-picker.min.css";
+const color:ewColorPicker = new ewColorPicker({
+    el:"#app"
+})
+console.log(color)
---
+++ example/ew-color-picker-demo/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+module.exports = {
+    entry:"./main.ts",
+    output:{
+        filename:"main.js",
+        path:path.resolve(__dirname,"dist")
+    },
+    mode:"development",
+    devServer:{
+        port:8081,
+        open:true
+    },
+    module:{
+        rules:[
+            {
+                test:/\.tsx?$/,
+                use:"ts-loader",
+                exclude:/node_modules/
+            },
+            {
+
+                test:/\.less?$/,
+                use:["style-loader","css-loader","less-loader"],
+                exclude:/node_modules/
+            },
+            {
+
+                test:/\.css?$/,
+                use:["style-loader","css-loader"]
+            }
+        ]
+    },
+    resolve:{
+        extensions:[".ts",".js"]
+    }
+}
