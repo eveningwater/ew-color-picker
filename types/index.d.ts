3a16f6fe311e60d83d364e5068991be912f901da
eveningwater <854806732@qq.com>
2021-12-04 16:54:50 +0800

    修改了类型定义

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,9 @@
 declare namespace ewColorPicker {
   export interface PredefineColorType {
-    color?: String;
-    disabled?: boolean;
+    color: String;
+    disabled: boolean;
   }
-  export type WrapperType =
+  export type ColorPickerElementType =
     | string
     | HTMLElement
     | Element
@@ -12,44 +12,40 @@ declare namespace ewColorPicker {
     | NodeList
     | null
     | void;
-  export type OptionType = {
-    el?: WrapperType;
-    alpha?: boolean;
-    hue?: boolean;
-    size?:
-      | string
-      | {
-          width?: string | number;
-          height?: string | number;
-        };
-    predefineColor?: string[] | Array<PredefineColorType>;
-    disabled?: boolean;
-    defaultColor?: string;
-    pickerAnimation?: string;
-    pickerAnimationTime?: number;
-    sure?: Function;
-    clear?: Function;
-    isLog?: boolean;
-    togglePicker?: Function;
-    changeColor?: Function;
-    hasBox?: boolean;
-    isClickOutside?: boolean;
-    hasClear?: boolean;
-    hasSure?: boolean;
-    hasInput?: boolean;
-    boxDisabled?: boolean;
-    openChangeColorMode?: boolean;
-    boxBgColor?: boolean;
-    hueDirection?: string;
-    alphaDirection?: string;
-    lang?:string;
-    userDefineText?:boolean;
-    clearText?:string;
-    sureText?:string;
+  type SizeType = string | Partial<{ width:string | number,height:string | number }>
+  export type ColorPickerOptionType = {
+    el: ColorPickerElementType;
+    alpha: boolean;
+    hue: boolean;
+    size:SizeType;
+    predefineColor: string[] | Array<Partial<PredefineColorType>>;
+    disabled: boolean;
+    defaultColor: string;
+    pickerAnimation: string;
+    pickerAnimationTime: number;
+    sure: Function;
+    clear: Function;
+    isLog: boolean;
+    togglePicker: Function;
+    changeColor: Function;
+    hasBox: boolean;
+    isClickOutside: boolean;
+    hasClear: boolean;
+    hasSure: boolean;
+    hasInput: boolean;
+    boxDisabled: boolean;
+    openChangeColorMode: boolean;
+    boxBgColor: boolean;
+    hueDirection: string;
+    alphaDirection: string;
+    lang:string;
+    userDefineText:boolean;
+    clearText:string;
+    sureText:string;
   };
 }
-export type WrapperType = ewColorPicker.WrapperType;
-export type OptionType = ewColorPicker.OptionType;
+export type ElementType = ewColorPicker.ColorPickerElementType;
+export type OptionType = Partial<ewColorPicker.ColorPickerOptionType>;
 export interface $DomType {
   box?: HTMLDivElement;
   hueBar?: HTMLDivElement;
@@ -69,12 +65,12 @@ export interface Instance {
   $Dom: $DomType;
   config: OptionType;
 }
+type getDefaultConfig = () => OptionType;
 declare class ewColorPicker implements Instance {
   public config: OptionType;
   public $Dom: $DomType;
-  constructor(colorPickerOption: WrapperType | OptionType);
-  createColorPicker(colorPickerOption: WrapperType | OptionType);
-  getDefaultConfig();
+  static createColorPicker:(colorPickerOption: ElementType | OptionType) => Instance;
+  static getDefaultConfig:() => ReturnType<getDefaultConfig>;
+  constructor(colorPickerOption: ElementType | OptionType);
 }
-
 export default ewColorPicker;
